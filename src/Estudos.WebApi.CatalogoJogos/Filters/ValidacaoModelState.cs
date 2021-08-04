cbac2e5f6c75515256f0c2ca4e7241c4fd4cdcec
jorgelucas <jorgelucasl91@gmail.com>
2021-08-04 17:53:17 -0300

    [FEATURE] Configurando o comportamento de validação do ModelState

---
+++ src/Estudos.WebApi.CatalogoJogos/Filters/ValidacaoModelState.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Estudos.WebApi.CatalogoJogos.Filters
+{
+    public class ValidacaoModelState : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(ObterErros(context.ModelState));
+            }
+        }
+
+        private ValidationProblemDetails ObterErros(ModelStateDictionary modelState)
+        {
+            var keys = modelState.Keys.Distinct();
+            var dictionary = new Dictionary<string, string[]>();
+            foreach (var key in keys)
+            {
+                dictionary[key] = modelState[key].Errors
+                    .Select(erro => erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message)
+                    .ToArray();
+            }
+            return ObterErrosResposta(dictionary);
+        }
+
+
+
+        private ValidationProblemDetails ObterErrosResposta(Dictionary<string, string[]> erros)
+        {
+            return new ValidationProblemDetails(erros)
+            {
+                Title = "Erros de validação encontrados"
+            };
+        }
+    }
+
+}
--- src/Estudos.WebApi.CatalogoJogos/Startup.cs
+++ src/Estudos.WebApi.CatalogoJogos/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,6 +21,12 @@ namespace Estudos.WebApi.CatalogoJogos
         {
 
             services.AddControllers();
+
+            services.Configure<ApiBehaviorOptions>(opt =>
+            {
+                opt.SuppressModelStateInvalidFilter = true;
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Estudos.WebApi.CatalogoJogos", Version = "v1" });
