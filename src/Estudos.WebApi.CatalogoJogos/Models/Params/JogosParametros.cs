1292010813d6362bb723880bf98ff1585a587101
jorgelucas <jorgelucasl91@gmail.com>
2021-08-05 17:06:59 -0300

    [BUGFIX] Corrigindo erro ao consupar pello nome ou produtora

--- src/Estudos.WebApi.CatalogoJogos/Models/Params/JogosParametros.cs
+++ src/Estudos.WebApi.CatalogoJogos/Models/Params/JogosParametros.cs
@@ -8,19 +8,24 @@ namespace Estudos.WebApi.CatalogoJogos.Models.Params
 {
     public class JogosParametros : IQuerySort, IQueryPaging
     {
+        [QueryOperator(Operator = WhereOperator.Contains)]
         [FromQuery(Name = "nome")]
         public string Nome { get; set; }
-        
+
+        [QueryOperator(Operator = WhereOperator.Contains)]
         [FromQuery(Name = "produtora")]
         public string Produtora { get; set; }
 
-        [QueryOperator(Operator = WhereOperator.GreaterThanOrEqualTo, HasName = "Preco"), FromQuery(Name = "preco_minimo")]
+        [QueryOperator(Operator = WhereOperator.GreaterThanOrEqualTo, HasName = "Preco")]
+        [FromQuery(Name = "preco_minimo")]
         public double? PrecoMinimo { get; set; }
 
-        [QueryOperator(Operator = WhereOperator.LessThanOrEqualTo, HasName = "Preco"), FromQuery(Name = "preco_maximo")]
+        [QueryOperator(Operator = WhereOperator.LessThanOrEqualTo, HasName = "Preco")]
+        [FromQuery(Name = "preco_maximo")]
         public double? PrecoMaximo { get; set; }
 
-        [QueryOperator(Operator = WhereOperator.Equals, HasName = "Preco"), FromQuery(Name = "preco")]
+        [QueryOperator(Operator = WhereOperator.Equals, HasName = "Preco")]
+        [FromQuery(Name = "preco")]
         public double? Preco { get; set; }
         public int? Offset { get; set; }
         public int? Limit { get; set; } = 10;
