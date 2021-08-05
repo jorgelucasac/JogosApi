2e89043acac6a38f0e6f04c9e5998383d4f959c3
jorgelucas <jorgelucasl91@gmail.com>
2021-08-05 16:24:47 -0300

    [FEATURE] COnfigurando acesso ao banco de dados
    - instalado o EF Core
    - criado as migrations
    - criado a classe de contexto

---
+++ src/Estudos.WebApi.CatalogoJogos/Data/JogosContext.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using Estudos.WebApi.CatalogoJogos.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Estudos.WebApi.CatalogoJogos.Data
+{
+    //Add-Migration Jogos -OutputDir "Data/Migrations"
+    public class JogosContext : DbContext
+    {
+        public JogosContext(DbContextOptions<JogosContext> options) : base(options) { }
+
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(JogosContext).Assembly);
+            MapearEntidadesEsquecidas(modelBuilder);
+
+            modelBuilder.Entity<Jogo>()
+                .HasQueryFilter(a => a.Ativo);
+        }
+
+        
+
+        public DbSet<Jogo> Jogos { get; set; }
+
+        private void MapearEntidadesEsquecidas(ModelBuilder builder)
+        {
+            foreach (var entity in builder.Model.GetEntityTypes())
+            {
+
+                var propriedadesString = entity
+                    .GetProperties().Where(p => p.ClrType == typeof(string));
+
+                var propriedadesDecimais = entity
+                    .GetProperties().Where(p => p.ClrType == typeof(decimal)
+                                                || p.ClrType == typeof(decimal?));
+
+                foreach (var propriedade in propriedadesString)
+                {
+                    if (string.IsNullOrEmpty(propriedade.GetColumnType())
+                        && !propriedade.GetMaxLength().HasValue)
+                    {
+                        //propriedade.SetMaxLength(100);
+                        propriedade.SetColumnType("VARCHAR(100)");
+                    }
+                }
+
+
+                //foreach (var propriedade in propriedadesDecimais)
+                //{
+                //    if (!propriedade.GetPrecision().HasValue)
+                //        propriedade.SetPrecision(18);
+
+                //    if (!propriedade.GetScale().HasValue)
+                //        propriedade.SetScale(2);
+                //}
+
+            }
+        }
+    }
+}
---
+++ src/Estudos.WebApi.CatalogoJogos/Data/Migrations/20210805171626_Jogos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Estudos.WebApi.CatalogoJogos.Data.Migrations
+{
+    public partial class Jogos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jogo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Produtora = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Preco = table.Column<double>(type: "float", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jogo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jogo");
+        }
+    }
+}
