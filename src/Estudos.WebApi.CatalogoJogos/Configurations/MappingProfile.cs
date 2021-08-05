4abeadb1f34070b2996649378c96ca1ea55f005f
jorgelucas <jorgelucasl91@gmail.com>
2021-08-05 10:57:56 -0300

    [FEATURE] Configurando parametros de busca de jogos e AutoMaper

---
+++ src/Estudos.WebApi.CatalogoJogos/Configurations/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Estudos.WebApi.CatalogoJogos.Business.Models;
+using Estudos.WebApi.CatalogoJogos.Models.InputModels;
+using Estudos.WebApi.CatalogoJogos.Models.ViewModels;
+
+namespace Estudos.WebApi.CatalogoJogos.Configurations
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Jogo, JogoViewModel>();
+            CreateMap<Jogo, JogoPathInputModel>();
+
+            CreateMap<JogoInputModel, Jogo>();
+        }
+
+
+    }
+}
---
+++ src/Estudos.WebApi.CatalogoJogos/Models/Params/JogosParametros.cs
@@ -0,0 +1,29 @@
+﻿using AspNetCore.IQueryable.Extensions.Attributes;
+using AspNetCore.IQueryable.Extensions.Filter;
+using AspNetCore.IQueryable.Extensions.Pagination;
+using AspNetCore.IQueryable.Extensions.Sort;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Estudos.WebApi.CatalogoJogos.Models.Params
+{
+    public class JogosParametros : IQuerySort, IQueryPaging
+    {
+        [FromQuery(Name = "nome")]
+        public string Nome { get; set; }
+        
+        [FromQuery(Name = "produtora")]
+        public string Produtora { get; set; }
+
+        [QueryOperator(Operator = WhereOperator.GreaterThanOrEqualTo, HasName = "Preco"), FromQuery(Name = "preco_minimo")]
+        public double? PrecoMinimo { get; set; }
+
+        [QueryOperator(Operator = WhereOperator.LessThanOrEqualTo, HasName = "Preco"), FromQuery(Name = "preco_maximo")]
+        public int? PrecoMaximo { get; set; }
+
+        [QueryOperator(Operator = WhereOperator.Equals, HasName = "Preco"), FromQuery(Name = "preco")]
+        public double? Preco { get; set; }
+        public int? Offset { get; set; }
+        public int? Limit { get; set; } = 10;
+        public string Sort { get; set; }
+    }
+}
