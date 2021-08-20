d25bf07b77889f330c1a26fd6c65180a5f8012ea
jorgelucas <jorgelucasl91@gmail.com>
2021-08-20 17:23:34 -0300

    [FEATURE] Response Types padrão

---
+++ src/Estudos.WebApi.CatalogoJogos/Filters/RespondeNotFound.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Estudos.WebApi.CatalogoJogos.Filters
+{
+    public class ResponseNotFound : ProducesResponseTypeAttribute
+    {
+        public ResponseNotFound() : base(typeof(string), StatusCodes.Status404NotFound)
+        {
+        }
+    } 
+    public class ResponseBadRequest : ProducesResponseTypeAttribute
+    {
+        public ResponseBadRequest() : base(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)
+        {
+        }
+    }
+}
